9f614cf857880f30f1bff928bfa81485aba1b8ee
nmcspadden <nmcspadden@gmail.com>
2014-01-14 15:35:42 -0800

    Added new URL Provider for MCEdu

---
+++ MinecraftEdu/MinecraftEduURLProvider.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env/python
+#
+# Copyright 2013 Nick McSpadden
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from autopkglib import Processor, ProcessorError
+from operator import itemgetter
+import urllib2
+
+__all__ = ["MinecraftEduURLProvider"]
+
+
+VERSION_LIST_URL = ("http://www.minecraftedu.com/api/api.php?action=b_getversions&eduversiontype=2")
+
+DOWNLOAD_URL = ("http://minecraftedu.com/api/api.php?action=b_downloadversion&username=%s&password=%s&hashed=%s&versiontodownload=%s&build=%s&eduversiontype=2&releasetype=%s")
+
+TYPE_DEFAULT = "stable"
+HASHED_DEFAULT = "true"
+
+class MinecraftEduURLProvider(Processor):
+	description = "Provides URL to the latest MinecraftEdu release."
+	input_variables = {
+		"type": {
+			"required": False,
+			"description": ("Which branch to use.  Choose development or "
+							"stable.  Defaults to %s." % TYPE_DEFAULT)
+		},
+		"hashed": {
+			"required": False,
+			"description": ("Password is hashed: true or false. Defaults"
+							"to %s." % HASHED_DEFAULT)
+		},
+		"username": {
+			"required": True,
+			"description": ("Username for MinecraftEdu site.")
+		},
+		"password": {
+			"required": True,
+			"description": ("Password for MinecraftEdu site.  Can be cleartext or "
+							"hashed.  Use http://minecraftedu.com/api/api.php?action=gethash&username=username&password=password"
+							"to generate hashed password.")
+		}
+	}
+	output_variables = {
+		"url": {
+			"description": "URL to the latest MinecraftEdu release.",
+		},
+	}
+	
+	__doc__ = description
+	
+	def get_mcedu_url(self, type, hashed, username, password):
+		request = urllib2.Request(VERSION_LIST_URL)
+		request.add_header("x-requested-with", "XMLHttpRequest")
+		try:
+			url_handle = urllib2.urlopen(request)
+			version_output = url_handle.read()
+			url_handle.close()
+		except BaseException as e:
+			raise ProcessorError("Can't open %s: %s" % (base_url, e))
+
+		stable_releases = filter(lambda x: 'stable' in x,version_output.split("<version>"))
+		version_numbers = []
+		for item in stable_releases:
+			version_numbers.append(item[:-7].split("_"))
+		
+		version_numbers.sort(key = itemgetter(1))
+		# version_numbers[0][0] = latest stable version
+		# version_numbers[0][1] = latest stable build
+		
+		return DOWNLOAD_URL % (username, password, hashed, version_numbers[0][0], version_numbers[0][1], type)
+   
+	def main(self):
+		# Determine type, hashed, username and password.
+		type = self.env.get("type", TYPE_DEFAULT)
+		username = self.env.get("username")
+		password = self.env.get("password")
+		hashed = self.env.get("hashed", HASHED_DEFAULT)
+		
+		self.env["url"] = self.get_mcedu_url(
+			type, hashed, username, password)
+		self.output("Found URL %s" % self.env["url"])
+	
+
+if __name__ == "__main__":
+	processor = MinecraftEduURLProvider()
+	processor.execute_shell()
+	
