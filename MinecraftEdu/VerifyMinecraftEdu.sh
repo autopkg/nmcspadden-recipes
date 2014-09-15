d9234d81c66d24cf2a75ece97e0ed4d12975fcc2
Nick McSpadden <nmcspadden@gmail.com>
2014-09-15 10:53:44 -0700

    Fixed a silly error

--- MinecraftEdu/VerifyMinecraftEdu.sh
+++ MinecraftEdu/VerifyMinecraftEdu.sh
@@ -2,14 +2,14 @@
 
 #VerifyMinecraftEdu
 
-mcpath="/Library/Application Support/minecraftedu"
+mcpath="/Library/Application Support/minecraftedu/"
 homepath="$HOME/Library/Application Support/"
 endpath="$HOME/Library/Application Support/minecraftedu/"
 #the rsync command below will not behave as expected if the trailing slashes are missing!
 
 if [[ -e "$mcpath" ]]
 then
-	/usr/bin/rsync -rtuc "$homepath" "$endpath"
+	/usr/bin/rsync -rtuc "$mcpath" "$endpath"
 	#r - recursive
 	#t - preserve modification dates
 	#u - update the folder, don't copy unchanged files
