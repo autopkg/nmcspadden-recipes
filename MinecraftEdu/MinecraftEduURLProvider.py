a650407eace04334b52c7f60029c1058b279b332
Shea Craig <shea.craig@da.org>
2014-09-17 12:12:50 -0400

    Fix type conversion and sorting of version numbers.
    Specifically, the current devel version with number 1.710 build 5 drops
    the final 0 when converted to float, thus causing the URL to be
    incorrect and fail.
    
    Uses distutils.version.LooseVersion for casting the version number.
    
    Also, cleans up whitespace, and renames some variables to better match
    their function.

--- MinecraftEdu/MinecraftEduURLProvider.py
+++ MinecraftEdu/MinecraftEduURLProvider.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#	  http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 from autopkglib import Processor, ProcessorError
+from distutils.version import LooseVersion
 from operator import itemgetter
 import urllib2
 
@@ -29,75 +30,75 @@ TYPE_DEFAULT = "stable"
 HASHED_DEFAULT = "true"
 
 class MinecraftEduURLProvider(Processor):
-	description = "Provides URL to the latest MinecraftEdu release."
-	input_variables = {
-		"type": {
-			"required": False,
-			"description": ("Which branch to use.  Choose development or "
-							"stable.  Defaults to %s." % TYPE_DEFAULT)
-		},
-		"hashed": {
-			"required": False,
-			"description": ("Password is hashed: true or false. Defaults"
-							"to %s." % HASHED_DEFAULT)
-		},
-		"username": {
-			"required": True,
-			"description": ("Username for MinecraftEdu site.")
-		},
-		"password": {
-			"required": True,
-			"description": ("Password for MinecraftEdu site.  Can be cleartext or "
-							"hashed.  Use http://minecraftedu.com/api/api.php?action=gethash&username=username&password=password"
-							"to generate hashed password.")
-		}
-	}
-	output_variables = {
-		"url": {
-			"description": "URL to the latest MinecraftEdu release.",
-		},
-		"version": {
-			"description": "Version in the form of: download.build (i.e. 1.64.6).",
-		}
-	}
-	
-	__doc__ = description
-	
-	def get_mcedu_url(self, dl_type, hashed, username, password):
-		request = urllib2.Request(VERSION_LIST_URL)
-		request.add_header("x-requested-with", "XMLHttpRequest")
-		try:
-			url_handle = urllib2.urlopen(request)
-			version_output = url_handle.read()
-			url_handle.close()
-		except BaseException as e:
-			raise ProcessorError("Can't open %s: %s" % (base_url, e))
+    description = "Provides URL to the latest MinecraftEdu release."
+    input_variables = {
+        "type": {
+            "required": False,
+            "description": ("Which branch to use.  Choose development or "
+                            "stable.  Defaults to %s." % TYPE_DEFAULT)
+        },
+        "hashed": {
+            "required": False,
+            "description": ("Password is hashed: true or false. Defaults"
+                            "to %s." % HASHED_DEFAULT)
+        },
+        "username": {
+            "required": True,
+            "description": ("Username for MinecraftEdu site.")
+        },
+        "password": {
+            "required": True,
+            "description": ("Password for MinecraftEdu site.  Can be cleartext or "
+                            "hashed.  Use http://minecraftedu.com/api/api.php?action=gethash&username=username&password=password"
+                            "to generate hashed password.")
+        }
+    }
+    output_variables = {
+        "url": {
+            "description": "URL to the latest MinecraftEdu release.",
+        },
+        "version": {
+            "description": "Version in the form of: download.build (i.e. 1.64.6).",
+        }
+    }
 
-		stable_releases = filter(lambda x: dl_type in x,version_output.split("<version>"))
-		version_numbers = []
-		for item in stable_releases:
-			version_numbers.append(item[:-7].split("_"))
-		
-		version_numbers.sort(key = itemgetter(1))
-		# version_numbers[0][0] = latest stable version
-		# version_numbers[0][1] = latest stable build
-		separator = "."
-		self.env["version"] = separator.join(version_numbers[0])
-		return DOWNLOAD_URL % (username, password, hashed, version_numbers[0][0], version_numbers[0][1], dl_type)
-   
-	def main(self):
-		# Determine type, hashed, username and password.
-		dl_type = self.env.get("type", TYPE_DEFAULT)
-		username = self.env.get("username")
-		password = self.env.get("password")
-		hashed = self.env.get("hashed", HASHED_DEFAULT)
-		
-		self.env["url"] = self.get_mcedu_url(
-			dl_type, hashed, username, password)
-		self.output("Found URL %s" % self.env["url"])
-	
+    __doc__ = description
+
+    def get_mcedu_url(self, dl_type, hashed, username, password):
+        request = urllib2.Request(VERSION_LIST_URL)
+        request.add_header("x-requested-with", "XMLHttpRequest")
+        try:
+            url_handle = urllib2.urlopen(request)
+            version_output = url_handle.read()
+            url_handle.close()
+        except BaseException as e:
+            raise ProcessorError("Can't open %s: %s" % (base_url, e))
+        releases = filter(lambda x: dl_type in x,version_output.split("<version>"))
+        version_numbers = []
+        build_length = int(len(dl_type)) + 1 #length of 'stable' or 'development' + 1 for the '|'
+        for item in releases:
+            release = item[:-build_length].split("_")
+            # Convert version number to a distutils.version.LooseVersion, and
+            # the build to an int.
+            version_numbers.append((LooseVersion(release[0]), release[1]))
+        version_numbers.sort(reverse=True)
+        # version_numbers[0][0] = latest version
+        # version_numbers[0][1] = latest build
+        self.env["version"] = '%s.%s' % tuple(version_numbers[0])
+        return DOWNLOAD_URL % (username, password, hashed, version_numbers[0][0], version_numbers[0][1], dl_type)
+
+    def main(self):
+        # Determine type, hashed, username and password.
+        dl_type = self.env.get("type", TYPE_DEFAULT)
+        username = self.env.get("username")
+        password = self.env.get("password")
+        hashed = self.env.get("hashed", HASHED_DEFAULT)
+
+        self.env["url"] = self.get_mcedu_url(
+            dl_type, hashed, username, password)
+        self.output("Found URL %s" % self.env["url"])
+
 
 if __name__ == "__main__":
-	processor = MinecraftEduURLProvider()
-	processor.execute_shell()
-	
+    processor = MinecraftEduURLProvider()
+    processor.execute_shell()
