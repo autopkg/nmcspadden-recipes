505e75269e94d48bf8403a6951c05f9adf8f0d71
Shea Craig <shea.craig@da.org>
2014-12-16 15:52:29 -0500

    Initial working test.

---
+++ AppStoreApp/PkgBuildTester.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+#
+# Copyright 2014 Shea G. Craig
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""See docstring for PkgBuildTester class"""
+
+import os.path
+import FoundationPlist
+import subprocess
+import xml.etree.ElementTree as ET
+
+from autopkglib import Processor, ProcessorError
+
+
+__all__ = ["PkgBuildTester"]
+
+
+class PkgBuildTester(Processor):
+    """Calls autopkgserver to create a package."""
+    description = __doc__
+    input_variables = {
+        "pkg_build_name": {
+            "required": True,
+            "description": (
+                "A package request dictionary. See "
+                "Code/autopkgserver/autopkgserver for more details.")
+        },
+        "pkg_dir": {
+            "required": True,
+            "description": (
+                "A package request dictionary. See "
+                "Code/autopkgserver/autopkgserver for more details.")
+        },
+        "force_pkg_build": {
+            "required": False,
+            "description": (
+                "When set, this forces building a new package even if "
+                "a package already exists in the output directory with "
+                "the same identifier and version number. Defaults to False"),
+        },
+    }
+    output_variables = {
+        "pkgbuilt": {
+            "default": False,
+            "description": (
+                "True if a new package was actually requested to be built. "
+                "False if a package with the same filename, identifier and "
+                "version already exists and thus no package was built (see "
+                "'force_pkg_build' input variable."),
+        },
+    }
+
+    def xar_expand(self, source_path):
+        '''Uses xar to expand an archive'''
+        try:
+            xarcmd = ["/usr/bin/xar",
+                      "-x",
+                      "-C", self.env.get('RECIPE_CACHE_DIR'),
+                      "-f", source_path,
+                      "PackageInfo"]
+            proc = subprocess.Popen(xarcmd,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
+            (_, stderr) = proc.communicate()
+        except OSError as err:
+            raise ProcessorError("xar execution failed with error code %d: %s"
+                                 % (err.errno, err.strerror))
+        if proc.returncode != 0:
+            raise ProcessorError("extraction of %s with xar failed: %s"
+                                 % (source_path, stderr))
+
+    def check_for_package(self):
+        '''Build a packaging request, send it to the autopkgserver and get the
+        constructed package.'''
+
+        # Check for an existing flat package in the output dir and compare its
+        # identifier and version to the one we're going to build.
+        pkg_build_name = self.env['pkg_build_name']
+        pkg_dir = self.env['pkg_dir']
+        pkg_path = os.path.join(pkg_dir, pkg_build_name + '.pkg')
+        if os.path.exists(pkg_path) and not self.env.get("force_pkg_build"):
+            self.output("Package already exists at path %s." % pkg_path)
+            self.xar_expand(pkg_path)
+            packageinfo_file = os.path.join(self.env['RECIPE_CACHE_DIR'],
+                                            'PackageInfo')
+            if not os.path.exists(packageinfo_file):
+                raise ProcessorError(
+                    "Failed to parse existing package, as no PackageInfo "
+                    "file count be found in the extracted archive.")
+
+            tree = ET.parse(packageinfo_file)
+            root = tree.getroot()
+            local_version = root.attrib['version']
+            local_id = root.attrib['identifier']
+
+            if (local_version == self.env['version']
+                    and local_id == self.env['bundleid']):
+                self.output("Existing package matches version and identifier, "
+                            "not building.")
+                self.env["pkg_path"] = pkg_path
+                self.env['pkgbuilt'] = True
+                return
+
+
+    def main(self):
+        '''Look for an already built package.'''
+        self.check_for_package()
+
+
+if __name__ == '__main__':
+    PROCESSOR = PkgBuildTester()
+    PROCESSOR.execute_shell()
+
