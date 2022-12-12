a624f56072d92bf086592e51b447517db1e99a9d
Nick McSpadden <nmcspadden@meta.com>
2022-12-12 12:04:12 -0800

    Correctly distinguish beta vs. non-beta

--- Xcode/AppleURLSearcher.py
+++ Xcode/AppleURLSearcher.py
@@ -62,63 +62,45 @@ class AppleURLSearcher(URLGetter):
                 "variable 'result_output_var_name' or is assigned a default "
                 "of 'match.'"
             )
+        },
+        "beta_version": {
+            "description": (
+                "If this is a Beta version of Xcode, this will contain the "
+                "'beta X' string. For Release Candidates, it will be 'RC X'."
+                " For non-betas, this will be an empty string."
+            )
+        },
+        "beta_version_underscores": {
+            "description": (
+                "Same as beta_version, but with underscores instead of spaces."
+            )
         }
     }
 
 
-    def parse_beta_info(self, url):
+    def generate_beta_name(self, beta_string, xcode_item, underscores=False):
+        """Generate a string for beta or RC version"""
+        beta_number = xcode_item["displayName"].split(" ")[-1]
+        if beta_number and underscores:
+            return f"{beta_string}_{beta_number}"
+        elif beta_number:
+            return f"{beta_string} {beta_number}"
+        return ""
+
+
+    # xcode_item is a Dict defined in main()
+    def parse_beta_info(self, xcode_item):
         """Parse download url to set beta environment variables"""
-        self.output(f"Parsing for beta version with url: {url}")
-        split_url = url.split("/")
-        matched_item = split_url[-1]
-        if matched_item.endswith(".xip"):
-            matched_item = matched_item.replace(".xip", "")
-        split_matched_item = matched_item.split("_")
-        if "beta" in split_matched_item:
-            self.env["is_beta"] = True
-            if split_matched_item[-1].isnumeric():
-                self.env["beta_version"] = split_matched_item[-1]
-            else:
-                # Normalizing beta_version to 1
-                self.env["beta_version"] = "1"
-            self.output(f"beta_version: {self.env['beta_version']}")
+        self.env["is_beta"] = bool(self.env.get("BETA"))
+        self.env["beta_version"] = ""
+        if not self.env.get("BETA"):    
             return
-        # Check for Release Candidate versions
-        if "Release" in split_matched_item and "Candidate" in split_matched_item:
-            self.env["is_beta"] = True
-            if split_matched_item[-1].isnumeric():
-                self.env["beta_version"] = f"Release Candidate {split_matched_item[-1]}"
-            else:
-                self.env["beta_version"] = "Release Candidate"
-            self.output(f"beta_version: {self.env['beta_version']}")
-            return
-        self.env["is_beta"] = False
-        self.output(f"is_beta: {self.env['is_beta']}")
-
-
-    # This code is taken directly from URLTextSearcher
-    def search_for(self, content, re_pattern, flags=None):
-        """Get data from url and search for re_pattern"""
-        flag_accumulator = 0
-        if flags:
-            for flag in flags:
-                if flag in re.__dict__:
-                    flag_accumulator += re.__dict__[flag]
-
-        re_pattern = re.compile(re_pattern, flags=flag_accumulator)
-        # Output this to disk so I can search it later
-        with open(
-            os.path.join(
-                self.env["RECIPE_CACHE_DIR"], "downloads", "url_text.txt"
-            ),
-            "w",
-        ) as f:
-            f.write(content)
-        if match := re_pattern.search(content):
-            # return the last matched group with the dict of named groups
-            return match[(match.lastindex or 0)], match.groupdict()
-        else:
-            raise ProcessorError("No match found in URL")
+        if "beta" in xcode_item["displayName"].lower():
+            self.env["beta_version"] = self.generate_beta_name("beta", xcode_item)
+        elif "release candidate" in xcode_item["displayName"].lower():
+            self.env["beta_version"] = self.generate_beta_name("RC", xcode_item)
+            self.env["beta_version_underscores"] = self.generate_beta_name("RC", xcode_item, underscores=True)
+        self.output(f"Generated beta version string: {self.env['beta_version']}")
 
 
     def output_result(self, url):
@@ -126,7 +108,7 @@ class AppleURLSearcher(URLGetter):
         # The final entry is the highest one
         self.output(f"Full URL: {url}")
         self.env[self.env["result_output_var_name"]] = url
-        self.output_variables = {self.env["result_output_var_name"]: url}
+        self.output_variables = {self.env["result_output_var_name"]: url, self.env["beta_version"]: self.env["beta_version"]}
 
 
     def main(self):
@@ -174,6 +156,7 @@ class AppleURLSearcher(URLGetter):
                     ),
                     "remotePath": y["remotePath"],
                     "filename": y["filename"],
+                    "displayName": y["displayName"],
                     "full_url": url,
                 }
                 xcode_list.append(xcode_item)
@@ -188,7 +171,7 @@ class AppleURLSearcher(URLGetter):
         self.output(f"Found matching item: {match['filename']}")
         if not (full_url_match := match["full_url"]):
             raise ProcessorError("No matching URL found!")
-        self.parse_beta_info(full_url_match)
+        self.parse_beta_info(match)
         self.output_result(full_url_match)
 
 
